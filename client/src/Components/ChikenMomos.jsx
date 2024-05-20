5bd4ddb9eceadb4d48d3bdfaa8ca884b42700649
nabin japrel <xattrinabin122@gmail.com>
2024-05-20 16:31:23 +0545

    making icon

--- client/src/Components/ChikenMomos.jsx
+++ client/src/Components/ChikenMomos.jsx
@@ -20,7 +20,7 @@ function ChikenMomos() {
           <p className='flex py-2 gap-1'><img src={vector} alt="" className='h-[16px]' />
           <span className=' text-coustemOrange text-bold text-xl -mt-2'> 200</span>
           </p>
-          <span className='flex justify-center text-center font-normal text-gray-600 mt-1'>(Freshly Steamed Buff Mommos made from whole wheat and kneaded with beat Carotene Rich Vegetable Juice)</span>
+          <span className=' ml-7 flex justify-center text-center font-normal text-gray-600 mt-1'>(Freshly Steamed Buff Mommos made from whole wheat and kneaded with beat Carotene Rich Vegetable Juice)</span>
         </div>
         </div>
         <div className='flex flex-col items-center w-[350px] '>
@@ -40,14 +40,14 @@ function ChikenMomos() {
       <div className='mt-10 flex md:flex-row  lg:flex-row justify-between lg:w-[936px] lg:gap-48 md:gap-36 w-100% gap-10 flex-col' >
              <div className='flex flex-col   items-center w-[350px] '>
         <div className='flex flex-col  '>
-          <img className='w-[260px] h-[189px] top-[23px] mr-10 z=0' src={frymomo} alt="" />
+          <img className='w-[260px] h-[189px] top-[23px]  z=0' src={frymomo} alt="" />
         </div>
         <div className='flex-col flex justify-center items-center mt-5'>
           <p className='text-slate-800 font-bold text-xl'>Buff Fry Momo</p>
           <p className='flex py-2 gap-1'><img src={vector} alt="" className='h-[16px]' />
           <span className=' text-coustemOrange text-bold text-xl -mt-2'> 200</span>
           </p>
-          <span className='flex justify-center text-center font-normal text-gray-600 mt-1'>(Freshly Steamed Buff Mommos made from whole wheat and kneaded with beat Carotene Rich Vegetable Juice)</span>
+          <span className=' ml-7 flex justify-center text-center font-normal text-gray-600 mt-1'>(Freshly Steamed Buff Mommos made from whole wheat and kneaded with beat Carotene Rich Vegetable Juice)</span>
         </div>
         </div>
              <div className='flex flex-col   items-center w-[350px] '>
